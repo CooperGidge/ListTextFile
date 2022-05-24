da04c7cad281f0531ed5e4d44ed3b83216c31953
CooperGidge <98977687+CooperGidge@users.noreply.github.com>
2022-05-24 13:13:15 -0400

    finished comments

--- src/listtextfile/ListTextFile.java
+++ src/listtextfile/ListTextFile.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ListTextFile.java
  */
 package listtextfile;
 
@@ -98,37 +96,60 @@ public class ListTextFile {
      * @throws IOException
      */
     public static void addToList() throws IOException {
+        // Get the String the user wants to add to the list
         String addThis = JOptionPane.showInputDialog("What would you like to add to the list?");
-        
+        // Get the position in the list the user wants to add the String to
         String toHere = JOptionPane.showInputDialog("What number do you want to add the item to?");
-        
+        // Add the String to the position in the list
+        // (subtract 1 because arrays start from 0)
         items.add(Integer.parseInt(toHere)-1, addThis);
-        
+        // Save the ArrayList to the file
         saveFile();
     }
     
+    /**
+     * Removes an item from the list
+     * @throws IOException
+     */
     public static void removeFromList() throws IOException {
+        // Get the position number of the item the user wants to remove from the list
         String toHere = JOptionPane.showInputDialog("What number do you want to remove from the list?");
-        
+        // Remove that item from the list
+        // (subtract 1 because arrays start from 0)
         items.remove(Integer.parseInt(toHere)-1);
-        
+        // Save the ArrayList to the file
         saveFile();
     }
     
+    /**
+     * Converts the list into a String that can be printed and saved to the file
+     * @return returns a String representation of the list
+     */
     public static String currentList() {
+        // Create an empty String to start
         String currentList = "";
+        // Add each item of the ArrayList to the String, adding a line break at the end of each
         for (int i = 0; i < items.size(); i++) {
             currentList += (items.get(i) + "\n");
         }
+        // Return the String representation of the list
         return currentList;
     }
     
+    /**
+     * Saves the ArrayList to the file
+     * @throws IOException
+     */
     public static void saveFile() throws IOException {
+        // Gets the ArrayList as a String representation
         String newFile = (currentList());
-        
+        // Open the file
         PrintWriter fileOut = new PrintWriter(new FileWriter("list.txt"));
+        // Replace the contents of the file with the String representation of the ArrayList
         fileOut.println(newFile);
+        // Close the file
         fileOut.close();
+        // Rebuild the ArrayList using the new file
         buildList();
     }
     
